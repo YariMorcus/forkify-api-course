e1d1fe8b37be5f91a9cc0f77caba426583f676ac
YariMorcus <yarimorcus@zeelandnet.nl>
2023-09-29 13:40:15 +0200

    Change API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'f7c41dd0-3c38-4b69-97f3-28d4b221e5ed';
+export const API_KEY = 'bb2c847b-5a4b-455d-b5cb-88f491c725b4';
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const CLOSE_RECIPE_EDITOR_SEC = 2.5;
