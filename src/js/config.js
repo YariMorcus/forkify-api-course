293aff0280cb886e6f892cedb1c3c7bc73aef6f1
YariMorcus <yarimorcus@zeelandnet.nl>
2023-09-29 13:25:41 +0200

    Reorganize code

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
+export const API_KEY = 'f7c41dd0-3c38-4b69-97f3-28d4b221e5ed';
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const API_KEY = 'f7c41dd0-3c38-4b69-97f3-28d4b221e5ed';
 export const CLOSE_RECIPE_EDITOR_SEC = 2.5;
+export const RES_PER_PAGE = 10;
